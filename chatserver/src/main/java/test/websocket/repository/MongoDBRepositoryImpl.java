90da6d9775b47171d830c8022902c24abffa43a1
JoeGukseon <zipcks1381@gmail.com>
2023-06-14 23:38:05 +0900

    :recycle: refactor : MongoDBRepositoryImpl 수정 - findNotReadMessagesByRoomId 생성 - aggregate로 쿼리문 생성

--- chatserver/src/main/java/test/websocket/repository/MongoDBRepositoryImpl.java
+++ chatserver/src/main/java/test/websocket/repository/MongoDBRepositoryImpl.java
@@ -2,6 +2,7 @@ package test.websocket.repository;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
+import org.springframework.data.mongodb.core.aggregation.*;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.core.query.Update;
@@ -15,6 +16,9 @@ import java.time.Duration;
 import java.time.Instant;
 import java.time.LocalDateTime;
 
+import static org.springframework.data.mongodb.core.aggregation.Aggregation.project;
+import static org.springframework.data.mongodb.core.query.Criteria.where;
+
 @RequiredArgsConstructor
 public class MongoDBRepositoryImpl implements MongoDBRepositoryCustom{
     private final ReactiveMongoTemplate mongoTemplate;
@@ -22,9 +26,9 @@ public class MongoDBRepositoryImpl implements MongoDBRepositoryCustom{
     @Override
     public Mono<Void> pushMessage(String roomId, ChatData chatData) {
         return mongoTemplate.updateFirst(
-                Query.query(Criteria.where("roomId").is(roomId)),
-                new Update().push("messages", chatData)
-                        .set("lastTime",chatData.getCurTime()), ChatRoom.class)
+                        Query.query(where("roomId").is(roomId)),
+                        new Update().push("messages", chatData)
+                                .set("lastTime",chatData.getCurTime()), ChatRoom.class)
                 .then();
     }
 
@@ -36,13 +40,13 @@ public class MongoDBRepositoryImpl implements MongoDBRepositoryCustom{
 ////                new Update().addToSet("users.$", user), ChatRoom.class)
 //                new Update().set("users.$", user), ChatRoom.class)
 //                .then();
-                Query.query(Criteria.where("roomId").is(roomId).and("users.email").is(user.getEmail())),
-                new Update().set("users.$", user),
-                ChatRoom.class)
+                        Query.query(where("roomId").is(roomId).and("users.email").is(user.getEmail())),
+                        new Update().set("users.$", user),
+                        ChatRoom.class)
                 .flatMap(updateResult -> {
                     if (updateResult.getModifiedCount() == 0) {
                         return mongoTemplate.updateFirst(
-                                Query.query(Criteria.where("roomId").is(roomId)),
+                                Query.query(where("roomId").is(roomId)),
                                 new Update().addToSet("users", user),
                                 ChatRoom.class);
                     }
@@ -53,7 +57,7 @@ public class MongoDBRepositoryImpl implements MongoDBRepositoryCustom{
     @Override
     public Mono<Void> deleteCheckList(String roomId, String email, String uuid) {
         return mongoTemplate.updateFirst(
-                        Query.query(Criteria.where("roomId").is(roomId).and("messages._id").is(uuid)),
+                        Query.query(where("roomId").is(roomId).and("messages._id").is(uuid)),
                         new Update().pull("messages.$.checkList", email), ChatRoom.class)
                 .then();
     }
@@ -61,12 +65,27 @@ public class MongoDBRepositoryImpl implements MongoDBRepositoryCustom{
     @Override
     public Mono<Void> updateLastTime(String roomId, String email) {
         Instant startTime = Instant.now();
-        Query query = Query.query(Criteria.where("roomId").is(roomId).and("users.email").is(email));
+        Query query = Query.query(where("roomId").is(roomId).and("users.email").is(email));
         Update update = new Update().set("users.$.lastCheckTime", LocalDateTime.now());
         return mongoTemplate.updateFirst(query, update, ChatRoom.class).then()
                 .doOnSuccess(v -> {
                     Duration duration = Duration.between(startTime, Instant.now());
                     System.out.println("Processing time: " + duration.toMillis() + " milliseconds");
                 });
+    }
+
+    @Override
+    public Mono<Integer> findNotReadMessagesByRoomId(String roomId, LocalDateTime afterTime) {
+        MatchOperation match = Aggregation.match(Criteria.where("_id").is(roomId));
+        ProjectionOperation project = project().andExclude("_id").and(ArrayOperators.Filter.filter("messages")
+                        .as("message")
+                        .by(ComparisonOperators.Gt.valueOf("$$message.curTime").greaterThanValue(afterTime)))
+                .as("messages");
+        TypedAggregation<ChatRoom> aggregation = Aggregation.newAggregation(ChatRoom.class, match, project);
+
+        return mongoTemplate.aggregate(aggregation, ChatRoom.class)
+                .singleOrEmpty()
+                .map(chatRoom -> chatRoom.getMessages().size())
+                .defaultIfEmpty(0);
     }
 }
